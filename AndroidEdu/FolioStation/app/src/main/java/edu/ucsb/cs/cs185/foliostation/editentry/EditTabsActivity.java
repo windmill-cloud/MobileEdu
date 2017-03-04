c7395cb0d58ce8df05d1f72e6a46849cb96b218d
keyrrae <xuan-wang@outlook.com>
2017-03-03 22:03:31 -0800

    revert the last change

--- AndroidEdu/FolioStation/app/src/main/java/edu/ucsb/cs/cs185/foliostation/editentry/EditTabsActivity.java
+++ AndroidEdu/FolioStation/app/src/main/java/edu/ucsb/cs/cs185/foliostation/editentry/EditTabsActivity.java
@@ -213,4 +213,49 @@ public class EditTabsActivity extends AppCompatActivity {
             }
         });
     }
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    mFragment = new SelectCoverFragment();
+                    return mFragment;
+                case 1:
+                    mFragment = new EditInfoFragment();
+                    return mFragment;
+            }
+            return null;
+                //return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
 }
