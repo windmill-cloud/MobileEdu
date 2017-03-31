e1794e9859fca5c64b5002fe03081147def69895
keyrrae <xuan-wang@outlook.com>
2017-03-31 00:25:48 -0700

    .

--- AndroidEdu/AppsForDemo/SensorsDemo/app/src/main/java/edu/ucsb/xuanwang/sensorsdemo/MainActivity.java
+++ AndroidEdu/AppsForDemo/SensorsDemo/app/src/main/java/edu/ucsb/xuanwang/sensorsdemo/MainActivity.java
@@ -1,13 +1,41 @@
 package edu.ucsb.xuanwang.sensorsdemo;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int i = 0;
+        Log.i("sensor", String.valueOf(event.values[0]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
