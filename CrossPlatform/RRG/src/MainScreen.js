05c1d37d586e16b40a843c5dd43674b08ab5d41b
keyrrae <xuan-wang@outlook.com>
2017-04-04 11:50:45 -0700

    bottom navigate bar

--- CrossPlatform/RRG/src/MainScreen.js
+++ CrossPlatform/RRG/src/MainScreen.js
@@ -3,23 +3,95 @@
  */
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import { NavigationComponent } from 'react-native-material-bottom-navigation'
+import { TabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-class MainScreen extends React.Component {
-
+class MoviesAndTV extends React.Component {
     static navigationOptions = {
-        title: 'Welcome',
+        tabBar: {
+            label: 'Movies & TV',
+            icon: () => (<Icon size={24} color="white" name="tv" />)
+        }
     };
 
     render() {
-        const { navigate } = this.props.navigation;
         return (
             <View>
-                <Text>Hello</Text>
-                <Button title="Button" onPress={ () => navigate('Chat') }/>
+                <Text>
+                    Movies and TV
+                </Text>
             </View>
         );
     }
 }
+
+class Music extends React.Component {
+    static navigationOptions = {
+        tabBar: {
+            label: 'Music',
+            icon: () => (<Icon size={24} color="white" name="music-note" />)
+        }
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    Music
+                </Text>
+            </View>
+        );
+    }
+}
+
+class Newsstand extends React.Component {
+    static navigationOptions = {
+        tabBar: {
+            label: 'Newsstand',
+            icon: () => (<Icon size={24} color="white" name="nature" />)
+        }
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    nature
+                </Text>
+            </View>
+        );
+    }
+}
+
+const MainScreen = TabNavigator({
+    MoviesAndTV: { screen: MoviesAndTV },
+    Music: { screen: Music },
+    Newsstand: { screen: Newsstand }
+}, {
+    tabBarComponent: NavigationComponent,
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+        bottomNavigationOptions: {
+            labelColor: 'white',
+            rippleColor: 'white',
+            tabs: {
+                MoviesAndTV: {
+                    barBackgroundColor: '#37474F'
+                },
+                Music: {
+                    barBackgroundColor: '#00796B'
+                },
+                Newsstand: {
+                    barBackgroundColor: '#EEEEEE',
+                    labelColor: '#434343', // like in the standalone version, this will override the already specified `labelColor` for this tab
+                    activeLabelColor: '#212121',
+                    activeIcon: <Icon size={24} color="#212121" name="newsstand" />
+                }
+            }
+        }
+    }
+});
 
 const styles = {
     containerStyle: {
