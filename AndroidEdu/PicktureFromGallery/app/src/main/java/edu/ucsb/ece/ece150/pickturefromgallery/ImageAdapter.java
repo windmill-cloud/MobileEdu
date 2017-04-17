bc98ab15f468616dcf1cdc9d37922479fb715c3d
keyrrae <xuan-wang@outlook.com>
2017-04-16 17:14:46 -0700

    .

--- AndroidEdu/PicktureFromGallery/app/src/main/java/edu/ucsb/ece/ece150/pickturefromgallery/ImageAdapter.java
+++ AndroidEdu/PicktureFromGallery/app/src/main/java/edu/ucsb/ece/ece150/pickturefromgallery/ImageAdapter.java
@@ -22,35 +22,24 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.post
     };
 
-    public ImageAdapter(Context c) {
-        mContext = c;
-    }
 
+    @Override
     public int getCount() {
-        return mThumbIds.length;
+        return 0;
     }
 
-    public Object getItem(int position) {
-        return mThumbIds[position];
+    @Override
+    public Object getItem(int i) {
+        return null;
     }
 
-    public long getItemId(int position) {
-        return position;
+    @Override
+    public long getItemId(int i) {
+        return 0;
     }
 
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(360, 360));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(2,2,2,2);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
     }
 }
