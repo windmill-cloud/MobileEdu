de2d8c1d9db182e1157b32c82aa91e5e05785815
keyrrae <xuan-wang@outlook.com>
2017-04-23 16:14:20 -0700

    .

--- AndroidEdu/XuanWangDemoSuite/app/src/main/java/edu/ucsb/cs/cs190i/xuanwang/xuanwangdemosuite/ComicFragment.java
+++ AndroidEdu/XuanWangDemoSuite/app/src/main/java/edu/ucsb/cs/cs190i/xuanwang/xuanwangdemosuite/ComicFragment.java
@@ -1,29 +1,64 @@
 package edu.ucsb.cs.cs190i.xuanwang.xuanwangdemosuite;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.almeros.android.multitouch.MoveGestureDetector;
+import com.almeros.android.multitouch.RotateGestureDetector;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ComicFragment extends SavableFragment {
+public class ComicFragment extends SavableFragment implements View.OnTouchListener {
 
+  ImageView mImageView;
+  Bitmap mBitMap;
+  Matrix mMatrix;
+
+  private float mScaleFactor = 1.0f;
+  private float mRotationDegrees = 0.f;
+  private float mFocusX = 0.f;
+  private float mFocusY = 0.f;
+
+  private ScaleGestureDetector mScaleDetector;
+  private RotateGestureDetector mRotateDetector;
+  private MoveGestureDetector mMoveDetector;
 
   public ComicFragment() {
     // Required empty public constructor
   }
 
-
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_comic, container, false);
+    View rootView = inflater.inflate(R.layout.fragment_comic, container, false);
+    mMatrix = new Matrix();
+
+    mImageView = (ImageView) rootView.findViewById(R.id.imageView);
+    mImageView.setOnTouchListener(this);
+    // Setup Gesture Detectors
+    mScaleDetector = new ScaleGestureDetector(getContext(), new ScaleListener());
+    mRotateDetector = new RotateGestureDetector(getContext(), new RotateListener());
+    mMoveDetector = new MoveGestureDetector(getContext(), new MoveListener());
+    return rootView;
   }
 
   @Override
@@ -35,4 +70,92 @@ public class ComicFragment extends SavableFragment {
   public void restoreState(Bundle bundle) {
 
   }
+
+  @Override
+  public boolean onTouch(View view, MotionEvent motionEvent) {
+    Drawable d = mImageView.getDrawable();
+    if(mBitMap == null){
+      mBitMap = drawableToBitmap(d);
+    }
+
+    mScaleDetector.onTouchEvent(motionEvent);
+    mRotateDetector.onTouchEvent(motionEvent);
+    mMoveDetector.onTouchEvent(motionEvent);
+
+    mImageView.setScaleType(ImageView.ScaleType.MATRIX);
+
+    RectF imageRectF = new RectF(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+    RectF viewRectF = new RectF(0, 0, mImageView.getWidth(), mImageView.getHeight());
+    mMatrix.setRectToRect(imageRectF, viewRectF, Matrix.ScaleToFit.CENTER);
+    mMatrix.preScale(mScaleFactor, mScaleFactor, mBitMap.getWidth()/2, mBitMap.getHeight()/2);
+
+    mMatrix.preRotate(mRotationDegrees, mBitMap.getWidth() / 2, mBitMap.getHeight() / 2);
+    mMatrix.postTranslate(mFocusX, mFocusY);
+
+    mImageView.setImageMatrix(mMatrix);
+
+    return true;
+  }
+
+  private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+      mScaleFactor *= detector.getScaleFactor(); // average distance between fingers
+      return true;
+    }
+
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+      return super.onScaleBegin(detector);
+    }
+
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+      super.onScaleEnd(detector);
+    }
+  }
+
+  private class RotateListener extends RotateGestureDetector.SimpleOnRotateGestureListener {
+    @Override
+    public boolean onRotate(RotateGestureDetector detector) {
+      mRotationDegrees -= detector.getRotationDegreesDelta();
+      return true;
+    }
+  }
+
+  private class MoveListener extends MoveGestureDetector.SimpleOnMoveGestureListener {
+    @Override
+    public boolean onMove(MoveGestureDetector detector) {
+      PointF d = detector.getFocusDelta();
+      mFocusX += d.x;
+      mFocusY += d.y;
+
+      // mFocusX = detector.getFocusX();
+      // mFocusY = detector.getFocusY();
+      return true;
+    }
+  }
+
+  public static Bitmap drawableToBitmap (Drawable drawable) {
+    Bitmap bitmap = null;
+
+    if (drawable instanceof BitmapDrawable) {
+      BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+      if(bitmapDrawable.getBitmap() != null) {
+        return bitmapDrawable.getBitmap();
+      }
+    }
+
+    if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+      bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+    } else {
+      bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+    }
+
+    Canvas canvas = new Canvas(bitmap);
+    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    drawable.draw(canvas);
+    return bitmap;
+  }
+
 }
--- AndroidEdu/XuanWangDemoSuite/app/src/main/java/edu/ucsb/cs/cs190i/xuanwang/xuanwangdemosuite/TextToSpeechFragment.java
+++ AndroidEdu/XuanWangDemoSuite/app/src/main/java/edu/ucsb/cs/cs190i/xuanwang/xuanwangdemosuite/TextToSpeechFragment.java
@@ -1,18 +1,25 @@
 package edu.ucsb.cs.cs190i.xuanwang.xuanwangdemosuite;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TextToSpeechFragment extends SavableFragment {
-
+  TextToSpeech textToSpeech;
 
   public TextToSpeechFragment() {
     // Required empty public constructor
@@ -23,7 +30,34 @@ public class TextToSpeechFragment extends SavableFragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_text_to_speech, container, false);
+
+    View rootView = inflater.inflate(R.layout.fragment_text_to_speech, container, false);
+
+    final EditText editText = (EditText) rootView.findViewById(R.id.editTextToSpeech);
+
+    textToSpeech = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+      @Override
+      public void onInit(int status) {
+        if (status != TextToSpeech.ERROR) {
+          textToSpeech.setLanguage(Locale.UK);
+        }
+      }
+    });
+
+    ImageButton speakButton = (ImageButton) rootView.findViewById(R.id.buttonTextToSpeech);
+    speakButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String toSpeak = editText.getText().toString();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+          textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null, null);
+        }else{
+          textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+        }
+      }
+    });
+
+    return rootView;
   }
 
   @Override
